3029fc49af448310439aa681b1f7c7f08155fd82
Spacefox95 <nathan.raynal@gmail.com>
2025-11-07 10:41:56 +0100

    ok

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,4 @@
 void main() {
   String str = "Holberton School";
-  List<String> listStr = str.split(' ');
-  print("$str$str$str\n${listStr[0]}\n");
+  print("$str$str$str\n${str.substring(0, 9)}\n");
 }
