0f6c343f9ccbbe1549ecf8bebd5148fc504aa2ae
Spacefox95 <nathan.raynal@gmail.com>
2025-11-28 14:52:03 +0100

    Static instanec

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,18 +1,28 @@
-class User {
+import '6-password.dart';
+
+class User extends Password {
   String name;
   int age;
   double height;
   int id;
+  String user_password;
 
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
+    required this.user_password,
   });
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'age': age, 'height': height};
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+      'user_password': user_password,
+    };
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
@@ -21,9 +31,11 @@ class User {
       name: userJson['name'] as String,
       age: userJson['age'] as int,
       height: userJson['height'] as double,
+      user_password: userJson['user_password'] as String,
     );
   }
 
+  @override
   String toString() {
     return 'User(id: ${id} ,name: ${name}, age: ${age}, height: ${height})';
   }
