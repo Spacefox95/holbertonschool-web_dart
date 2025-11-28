a3069f45fe3cf714c8bf08eb1965596b70f4b238
Spacefox95 <nathan.raynal@gmail.com>
2025-11-28 11:05:21 +0100

    Static instanec

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,30 @@
+class User {
+  String name;
+  int age;
+  double height;
+  int id;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'age': age, 'height': height};
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'] as int,
+      name: userJson['name'] as String,
+      age: userJson['age'] as int,
+      height: userJson['height'] as double,
+    );
+  }
+
+  String toString() {
+    return 'User(id: ${id},name: ${name}, age: ${age}, height: ${height})';
+  }
+}
